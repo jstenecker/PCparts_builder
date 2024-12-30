535dfb1c4873f3d781123904d5da69f012df7771
Joseph <jstenecker@gmail.com>
2024-12-30 12:59:47 -0500

    Added pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+//This serves as the home page for the website, giving brief description of its purpose
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to PC Parts builder!</h1>
+        </div>
+    );
+};
---
+++ src/pages/Login.jsx
@@ -0,0 +1,3 @@
+//Login and account creation page, utilizes Firebase for secure Google auth
+import React from "react";
+
