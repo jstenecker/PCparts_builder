cfe111a0efbd8eca652380df2ae2a0f42710b499
Will <wrdavis1917@gmail.com>
2024-12-30 13:15:26 -0500

    asdf

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Home = () => {
     return (
         <div>
-            <h1>Welcome to PC Pants asdf builder!</h1>
+            <h1>Welcome to PC Pants asasdfdf builder!</h1>
         </div>
     );
 };
